7783704cc1612cebf26d2d41be6b3e88055e1597
Mohammad Jahangiry <mjahangiry75@gmail.com>
2020-11-18 10:50:27 +0330

    Config database

--- src/main/kotlin/io/github/masterj3y/sorna/SornaApplication.kt
+++ src/main/kotlin/io/github/masterj3y/sorna/SornaApplication.kt
@@ -2,8 +2,10 @@ package io.github.masterj3y.sorna
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories
 class SornaApplication
 
 fun main(args: Array<String>) {
